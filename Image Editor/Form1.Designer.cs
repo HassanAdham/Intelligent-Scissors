e8b792e77829177511aa4c24fc92b41e60eb1835
dinaYriad <45600778+dinaYriad@users.noreply.github.com>
2018-12-05 00:26:55 +0200

    Dots and Lines

--- Image Editor/Form1.Designer.cs
+++ Image Editor/Form1.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 23);
+            this.button1.Location = new System.Drawing.Point(35, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 88);
+            this.button1.Size = new System.Drawing.Size(175, 108);
             this.button1.TabIndex = 0;
             this.button1.Text = "Open Image";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,23 +47,41 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(210, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(280, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(550, 398);
+            this.pictureBox1.Size = new System.Drawing.Size(733, 490);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(35, 143);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "On";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.EnabledChanged += new System.EventHandler(this.checkBox1_EnabledChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +89,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Image Editor/Form1.cs
+++ Image Editor/Form1.cs
@@ -18,6 +18,9 @@ namespace Image_Editor
         }
 
         RGBPixel[,] ImageMatrix;
+        int pointNumber = 0;
+        Point startPoint;
+        Point freePoint;
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -29,6 +32,59 @@ namespace Image_Editor
                 ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
                 ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
             }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (checkBox1.Checked==true)
+            {
+                pictureBox1_CreateDash(e.Location.X,e.Location.Y);
+                if (pointNumber == 0)
+                {
+                     pointNumber++;
+                     startPoint = new Point(e.Location.X,e.Location.Y);
+                }
+                else if(pointNumber == 1)
+                {
+                    pointNumber++;
+                    freePoint = new Point(e.Location.X, e.Location.Y);
+                    drawLine(startPoint, freePoint);
+                }
+                else
+                {
+                    startPoint = freePoint;
+                    freePoint = new Point(e.Location.X, e.Location.Y);
+                    drawLine(startPoint, freePoint);
+                }       
+            }     
+        }
+
+        private void checkBox1_EnabledChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+                checkBox1.Text = "On";
+            else
+                checkBox1.Text = "Off";
+        }
+
+        private void pictureBox1_CreateDash(int x, int y)
+        {
+            Label l = new Label();
+            l.Text = "-";
+            l.Size = new Size(3, 3);
+            l.BackColor = Color.Blue;
+            l.Location = new Point(x, y);
+
+            pictureBox1.Controls.Add(l);
+        }
+
+        private void drawLine(Point p1, Point p2)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Rectangle r = new Rectangle();
+            PaintEventArgs p = new PaintEventArgs(g,r);
+            Pen blackPen = new Pen(Color.Black, 3);
+            p.Graphics.DrawLine(blackPen, p1,p2);
         }
     }
 }
